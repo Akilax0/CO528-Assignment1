cba40f44b38f7f143cb9d74936616338f2b2b34b
Akilax0 <killerilaksha@gmail.com>
2024-08-27 07:07:10 +0530

    init

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify, abort
+
+
+app = Flask(__name__)
+
+
+books = []
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    return jsonify(books)
+
+
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    book = next((book for book in books if book['id'] == book_id),None)
+    if book is None:
+        abort(404)
+    return jsonify(book)
+        
+@app.route('/books', methods=['POST'])
+def add_book():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    book = {
+        'id': books[-1]['id'] + 1 if books else 1,
+        'title': request.json['title'],
+        'author': request.json.get('author',""),
+        'isbn': request.json.get('isbn',"")
+    }
+    books.append(book)
+    return jsonify(book), 201
+        
+        
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id): 
+    book = next((book for book in books if book['id'] == book_id),None)
+    if book in None:
+        abort(404)
+    if not request.json:
+        abort(400)
+
+    book['title'] = request.json.get('title',book['title'])    
+    book['author'] = request.json.get('author',book['author'])    
+    book['isbn'] = request.json.get('isbn',book['isbn'])    
+    
+    return jsonify(book)
+
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id): 
+    book = next((book for book in books if book['id'] ==  book_id),None)
+    if book is None:
+        abort(404)
+        
+    books.remove(book)
+    return '',204
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
